62a72f216ba60a2c8e8f6f71df2edb562934f1d4
Jose Farias <jose@37signals.com>
2024-10-17 17:08:45 -0600

    Stop splitting array params before persisting

--- app/controllers/buckets/views_controller.rb
+++ app/controllers/buckets/views_controller.rb
@@ -2,20 +2,22 @@ class Buckets::ViewsController < ApplicationController
   include BucketScoped
 
   def create
-    @bucket.views.create! filters: view_params.merge(assignee_ids:, tag_ids:).compact_blank
-    redirect_back_or_to bucket_bubbles_path(@bucket), notice: "✓"
+    @bucket_view = @bucket.views.create! filters: filter_params
+    redirect_to bucket_bubbles_path(@bucket, **filter_params), notice: "✓"
+  end
+
+  def update
+    @bucket_view.update! filters: filter_params
+    redirect_to bucket_bubbles_path(@bucket, **filter_params), notice: "✓"
+  end
+
+  def destroy
+    @bucket_view.destroy
+    redirect_to bucket_bubbles_path(@bucket, **filter_params), notice: "✓"
   end
 
   private
-    def view_params
-      params.require(:view).permit(:order_by, :status, :assignee_ids, :tag_ids)
-    end
-
-    def assignee_ids
-      view_params[:assignee_ids]&.split(",")
-    end
-
-    def tag_ids
-      view_params[:tag_ids]&.split(",")
+    def filter_params
+      helpers.bubble_filter_params.to_h.compact_blank
     end
 end

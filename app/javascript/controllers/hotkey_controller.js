880e3d98b2884a80fd719556ba35ff372a4598b6
Kevin McConnell <kevin@37signals.com>
2025-05-28 08:18:49 +0100

    Prevent default when handling keyboard shortcuts
    Without this, any default action would still happen. With Chrome on
    Linux, that causes browser features to be triggered with our CTRL+J and
    CTRL+K actions.

--- app/javascript/controllers/hotkey_controller.js
+++ app/javascript/controllers/hotkey_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   click(event) {
     if (this.#isClickable && !this.#shouldIgnore(event)) {
+      event.preventDefault()
       this.element.click()
     }
   }
